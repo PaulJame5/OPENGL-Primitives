7dfddc4ef7ba86dbe05e4924c5901c138d1b2015
PaulJame5 <43470494+PaulJame5@users.noreply.github.com>
2018-11-28 20:49:20 +0000

    Add files via upload

---
+++ Game.h
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <SFML/Window.hpp>
+#include <SFML/OpenGL.hpp>
+
+
+#include <gl/GL.h>
+#include <gl/GLU.h>
+
+using namespace std;
+using namespace sf;
+
+class Game
+{
+public:
+	Game();
+	~Game();
+	void run();
+private:
+	Window window;
+	bool isRunning = false;
+	void initialize();
+	void update();
+	void draw();
+	void unload();
+
+	 // Uncomment for Part 2
+	 // ********************
+	const int primatives;
+
+	GLuint index;
+	Clock clock;
+	Time elapsed;
+
+	int image = 0;
+	void glLoadIdentityOn();
+	bool identity = false;	
+	bool identityPress = false;
+
+	float rotationAngle = 0.0f;
+	bool pressed = false;
+	bool swapPress();
+	 // ********************
+};
